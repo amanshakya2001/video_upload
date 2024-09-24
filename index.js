e6f09e5dee84a535824cfb6e5b4bb7886444a31c
Aman <aman.shakya@analyticsvidhya.com>
2024-09-24 18:03:11 +0530

    .

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');  // express-fileupload को इंपोर्ट करें
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');  // cors को इंपोर्ट करें
 const app = express();
+
+// CORS मिडलवेयर का उपयोग करें
+app.use(cors());
+
+// express-fileupload मिडलवेयर का उपयोग करें
+app.use(fileUpload());
+
+// स्टैटिक फ़ोल्डर को सर्व करने के लिए
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Route to check if the server is running
 app.get('/', (req, res) => {
@@ -9,20 +20,42 @@ app.get('/', (req, res) => {
 });
 
 // Route to handle video chunk upload
+<<<<<<< Updated upstream
 app.post('/upload', (req, res) => {
   console.log(req)
   console.log("request",req.post.fileName,req.body)
   const filePath = path.join(__dirname, 'uploads', req.body.fileName);
+=======
+app.post('/upload/', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  const uploadsDir = path.join(__dirname, 'uploads');
+  
+  if (!fs.existsSync(uploadsDir)){
+    fs.mkdirSync(uploadsDir);
+  }
+
+  const filePath = path.join(uploadsDir, req.body.fileName);
+  console.log("filepath", filePath);
+  
+>>>>>>> Stashed changes
   const chunkIndex = parseInt(req.body.chunkIndex);
+  console.log("chunkIndex", chunkIndex);
+  
   const totalChunks = parseInt(req.body.totalChunks);
+  console.log("Total Chunk", totalChunks);
 
   // Append the chunk to the file
   const chunk = req.files.file.data;
+  console.log(chunk);
   fs.appendFileSync(filePath, chunk);
 
-  // If it's the last chunk, respond with a success message
+  // If it's the last chunk, respond with a success message and file URL
   if (chunkIndex + 1 === totalChunks) {
-    res.send({ status: 'complete' });
+    const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.body.fileName}`;
+    res.send({ status: 'complete', fileUrl: fileUrl });
   } else {
     res.send({ status: 'chunk received' });
   }
